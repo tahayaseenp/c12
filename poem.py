8603346977cae1105c5e9af1f987ab1f53d0d740
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-09-20 13:57:42 +0000

    Tue, Sep 20, 2022, 1:57 PM +00:00

--- poem.py
+++ poem.py
@@ -7,8 +7,10 @@
 #   Display "E" in place of all the occurence of "A"
 #       I SELL COMPUTARS. I HAVE A COMPUTAR. I NEED A COMPUTAR. I WANT A COMPUTAR. I USE THAT COMPUTAR. MY COMPUTAR CRASHED.
 
+import sys
 
-def func1():
+
+def lines():
     c = 0
     for i in open("poem.txt").readlines():
         if i[0].isupper() == True:
@@ -16,7 +18,7 @@ def func1():
     return "Lines starting from capital letters :", c
 
 
-def func2():
+def word():
     return "Number of times the word corona appears :", open("poem.txt").read().lower().count("corona")
 
 
@@ -37,16 +39,36 @@ def ATOEDISP1():
 def ATOEDISP2():
     return open("news.txt").read().replace("A", "E")
 
-print("""
-1. Count the number of lines in "POEM.txt" that begins using upper case
-2. Read lines from the file "POEM.txt" and count how many how many times the word "corona" exists in file
-3. Read the text file "POEM.txt" and display those lines that start with "S"
-4. Read the file "POEM.txt" and display size of the file
-5. Read the file "NEWS.txt" and display "E" in place of all the occurence of "A" in the word COMPUTER
-6. Read the file "NEWS.txt" and display "E" in place of all the occurence of "A"
-""")
+while True:
+    print("""
+    1. Count the number of lines in "POEM.txt" that begins using upper case
+    2. Read lines from the file "POEM.txt" and count how many how many times the word "corona" exists in file
+    3. Read the text file "POEM.txt" and display those lines that start with "S"
+    4. Read the file "POEM.txt" and display size of the file
+    5. Read the file "NEWS.txt" and display "E" in place of all the occurence of "A" in the word COMPUTER
+    6. Read the file "NEWS.txt" and display "E" in place of all the occurence of "A"
+    0. Exit
+    """)
 
-ch = int(input("Enter your choice: "))
+    ch = int(input("Enter your choice: "))
 
-if ch == 1:
-    
+    if ch == 1:
+        print(lines())
+
+    elif ch == 2:
+        print(word())
+
+    elif ch == 3:
+        dispS()
+
+    elif ch == 4:
+        print(COUNTSIZE())
+
+    elif ch == 5:
+        print(ATOEDISP1())
+
+    elif ch == 6:
+        print(ATOEDISP2())
+
+    elif ch == 0:
+        sys.exit("Thank You!")
