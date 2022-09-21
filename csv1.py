06b1550613a9cbac7018538660b7e313d263d4e4
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-09-21 04:52:16 +0000

    Wed, Sep 21, 2022, 4:52 AM +00:00

--- csv1.py
+++ csv1.py
@@ -1,6 +1,6 @@
 import csv
 f = open("test.csv", "w+")
-B = csv.writer(f)
-B.writerow(['id', 'name', 'email'])
-B.writerow([100, 'Ronald', 'ronald@comp.com'])
+w = csv.writer(f)
+w.writerow(['id', 'name', 'email'])
+w.writerow([100, 'Ronald', 'ronald@comp.com'])
 f.close()
