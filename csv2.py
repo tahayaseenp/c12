2f9b2a6d73e1b010a60c1b5799fb476b8ae08025
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-09-21 05:55:19 +0000

    Wed, Sep 21, 2022, 5:55 AM +00:00

--- csv2.py
+++ csv2.py
@@ -2,24 +2,29 @@
 import csv
 import sys
 
+def write():
+    def em_in():
+        while True:
+            emno = int(input("Enter employee ID: "))
+            name = input("Enter employee name: ")
+            salr = float(input("Enter employee salary: "))
+            dept = input("Enter employee department: ")
+            w.writerow([emno, name, salr, dept])
+            c = input("Would you like to continue? (y/n) : ")
+            if c.lower() != 'y':
+                break
 
-def em_in():
-    while True:
-        emno = int(input("Enter employee ID: "))
-        name = input("Enter employee name: ")
-        salr = float(input("Enter employee salary: "))
-        dept = input("Enter employee department: ")
-        w.writerow([emno, name, salr, dept])
-        c = input("Would you like to continue? (y/n)")
-        if c.lower() != 'y':
-            break
+    file = open("employee.csv", "a")
+    w = csv.writer(file)
+    w.writerow(["Employee ID", "Employee Name", "Employee Salary", "Employee Department"])
+    em_in()
+    file.close()
 
-file = open("employee.csv", "w")
-w = csv.writer(file)
-w.writerow(["Employee ID", "Employee Name", "Employee Salary", "Employee Department"])
-file.close()
-file = open("employee.csv", "a")
-w = csv.writer(file)
-em_in()
-file.close()
+
+def read():
+    file = open("employee.csv", "r")
+    r = csv.reader(file)
+    for i in r: print(i)
+
+
 sys.exit("Thank You!")
