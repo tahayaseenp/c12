d898d18e36d0af56a8a1b54a0e3fee655124a162
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-09-21 05:44:26 +0000

    Wed, Sep 21, 2022, 5:44 AM +00:00

--- csv2.py
+++ csv2.py
@@ -1,3 +1,23 @@
 # Write a program to accept the input of employees from the user and store it in a CSV file and retrieve it to display to the user
+import csv
+
+
 def input():
-    
+    while True:
+        emno = int(input("Enter employee ID: "))
+        name = input("Enter employee name: ")
+        salr = float(input("Enter employee salary: "))
+        dept = input("Enter employee department: ")
+        w.writerow([emno, name, salr, dept])
+        c = input("Would you like to continue? (y/n)")
+        if c.lower() != 'y':
+            break
+
+file = open("employee.csv", "w")
+w = csv.writer(file)
+w.writerow(["Employee ID", "Employee Name", "Employee Salary", "Employee Department"])
+file.close()
+file = open("employee.csv", "a")
+w = csv.writer(file)
+input()
+file.close()
