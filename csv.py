95619b167fb8cdce14fb4032d3a1adc9203da76b
Taha Yaseen <98612931+tahayaseenp@users.noreply.github.com>
2022-09-21 04:42:14 +0000

    Wed, Sep 21, 2022, 4:42 AM +00:00

--- csv.py
+++ csv.py
@@ -1,5 +1,5 @@
-"""
-4 mark question from CSV
-Fill in the blanks
-
-"""
+import csv
+f = open("test.csv", "w+")
+writer = f.writer()
+writer.writerow(['id', 'name', 'email'])
+writer.writerow([100, 'Ronald', 'ronald@comp.com'])
